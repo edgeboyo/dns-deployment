872e5dc8e0d16689ebb27147aeaaab2fdd37bdae
edgeboyo <mkolybko@gmail.com>
2022-03-21 10:52:01 +0000

    Switched up structrure and defaults

---
+++ ArgumentParser.py
@@ -0,0 +1,10 @@
+import argparse
+
+def prepParser():
+    parser = argparse.ArgumentParser(description='Start a DNS implemented in Python. Usually DNSs use UDP on port 53.')
+    parser.add_argument('--port', default=53, type=int, help='The port to listen on.')
+    parser.add_argument('--tcp', action='store_true', help='Listen to TCP connections.')
+    parser.add_argument('--udp', action='store_true', help='Listen to UDP datagrams.')
+
+    return parser
+
