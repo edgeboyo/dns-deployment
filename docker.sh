399e524bea3d083d647acfd0299f3404fdf2b939
edgeboyo <mkolybko@gmail.com>
2022-04-15 14:07:43 +0200

    Added docker build and create script

--- docker.sh
+++ docker.sh
@@ -8,4 +8,21 @@ then
     return 1
 fi
 
+for var in "$@"
+do
+    if [[ $var == '-b' ]] || [[ $var == '--build' ]]
+    then
+        docker build --tag dns-deployment .
+    elif [[ $var == '-c' ]] || [[ $var == '--create' ]] 
+    then
+        docker container create dns-deployment
+    else
+        echo "Unknown command: $var"
+        echo "Check usage..."
+        return 2
+    fi
+done
+
+ # use later
+
 # Going to finish this tomorrow
