8bd0cb704b44bf90f33c588f65795468c88c3d7e
edgeboyo <mkolybko@gmail.com>
2022-04-15 14:35:28 +0200

    Set up docker script fully

--- docker.sh
+++ docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "$#"
-
 if (( $# == 0 )) || [[ $1 == "-h" ]] || [[ $1 == "--help" ]]
 then
-    echo "Display help message"
+    echo "$0 -b/--build -c/--create -d/--deploy -h/--help"
+    echo "---------------------------------------------------------------"
+    echo "-b/--build - Build docker image from source"
+    echo "-c/--create - Create new docker container"
+    echo "-d/--deploy - Deploy image to docker repository (required login to repo)"
+    echo "-h/--help - Show this message"
+    echo "---------------------------------------------------------------"
     return 1
 fi
 
@@ -16,6 +20,10 @@ do
     elif [[ $var == '-c' ]] || [[ $var == '--create' ]] 
     then
         docker container create dns-deployment
+    elif [[ $var == '-d' ]] || [[ $var == '--deploy' ]]
+    then
+        docker tag dns-deployment registry.digitalocean.com/part3-project/dns-deployment
+        docker push registry.digitalocean.com/part3-project/dns-deployment
     else
         echo "Unknown command: $var"
         echo "Check usage..."
