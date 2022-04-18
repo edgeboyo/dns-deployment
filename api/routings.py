e613dc9a24e7cddab8cc9eaef3da8d545dbee193
edgeboyo <mkolybko@gmail.com>
2022-04-18 14:47:58 +0200

    Stuck on first POST request

--- api/routings.py
+++ api/routings.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, request
 
 tld = "tld"  # will be imported from config later
 
@@ -16,6 +16,15 @@ def return_json(object, code=200):
     return response
 
 
+def return_error(message, code=400):
+    error = {"message": message}
+
+    return return_json(error, code)
+
+
+# ROUTINGS BEGIN HERE
+
+
 @app.route("/")
 def gui_root():
     return "<p>This will be the GUI root later</p>"
@@ -28,8 +37,8 @@ def api_root():
     return return_json(data)
 
 
-@app.route("/api/domains/")
-def api_domains():
+@app.route("/api/domains/", methods=['GET', 'POST'])
+def api_domains_get():
     domains = []
     for file in os.listdir('./data'):
         if not os.fsdecode(file).endswith('.json'):
@@ -39,7 +48,7 @@ def api_domains():
 
         with open(filePath) as f:
             domainInfo = json.load(f)
-            domainName = domainInfo['name']
+            domainName = domainInfo['domainName']
 
         domain = {"domainName": domainName,
                   "resource": f"/api/domains/{domainName}", "uri": f"{domainName}.{tld}", "records": f"/api/domains/{domainName}/records"}
@@ -47,3 +56,30 @@ def api_domains():
         domains.append(domain)
 
     return return_json(domains)
+
+
+@app.route("/api/domains/", methods=[''])
+def api_domains_post():
+    data = request.get_json()
+
+    if not data:
+        return return_error("Data not formatted as JSON")
+
+    domainName = data['domainName']
+
+    if not domainName:
+        return return_error("`domainName` field missing in request")
+
+    filePath = f"./data/{domainName}.json"
+
+    if os.path.exists(filePath):
+        return return_error(f"Domain {filePath} already exists", 409)
+
+    with open(filePath, "w") as f:
+        domain = {"domainName": domainName, "records": {}}
+        json.dump(domain, f, indent=4)
+
+    domain = {"domainName": domainName,
+              "resource": f"/api/domains/{domainName}", "uri": f"{domainName}.{tld}", "records": f"/api/domains/{domainName}/records"}
+
+    return return_json(domain)
