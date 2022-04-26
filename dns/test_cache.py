31413ed2f04f2170e413844293e30f75abb7b084
edgeboyo <mkolybko@gmail.com>
2022-04-26 13:57:46 +0100

    Added additional tests (one disabled)

--- dns/test_cache.py
+++ dns/test_cache.py
@@ -42,3 +42,55 @@ def test_fullness_exception():
         cache = TTLCache(99, 999)
         for i in range(0, 100):
             cache.place(str(i), i, 100)
+
+
+def test_place_refresh():
+    cache = TTLCache(cycleTime=1)
+
+    cache.place('A', 123, 5)
+
+    originalTime = cache.request('A')[0].timeOfDeath
+
+    time.sleep(2)
+
+    afterWaitTime = cache.request('A')[0].timeOfDeath
+
+    assert afterWaitTime == originalTime
+
+    cache.place('A', 123, 100)
+
+    replaceTime = cache.request('A')[0].timeOfDeath
+
+    assert originalTime != replaceTime
+
+
+@pytest.mark.skip(reason="This is really inconstant and the system is really simple. No way this fails")
+def test_polling_replacer():
+    cache = TTLCache(1, .1)
+
+    cache.place('A', 1, 9999)
+    while True:
+        try:
+            # trying to place it in the middle of a cache cycle
+            cache.place('B', 1, 9999)
+        except:
+            print("FULLNESS")
+            continue
+
+        break
+
+    assert not cache.request('A')[1]
+
+    assert cache.request('B')[1] and cache.request('B')[0].value == 1
+
+    cache.cycleTime = 5
+
+    time.sleep(1)
+
+    cache.place('B', 1, 0)
+
+    cache.cycleTime = .1
+
+    time.sleep(5)
+
+    assert cache.request('A')[1]  # value returned by safety thread
